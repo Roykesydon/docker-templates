0a8204631bc29cf9df65b15860f3c706b9247305
Roykesydon <64363701+Roykesydon@users.noreply.github.com>
2022-03-16 17:08:18 +0800

    Update create_container.sh

--- tensorflow_jupyter/create_container.sh
+++ tensorflow_jupyter/create_container.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --user root -d -it --gpus device=0 -v $(pwd):/tf/workspace -p 11888:8888 jupyter/tensorflow-notebook bash
+docker run --user root -d -it --gpus device=0 -v $(pwd):/home/jovyan/work -p 11888:8888 jupyter/tensorflow-notebook bash
